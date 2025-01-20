48998253f4004c92cf1d5f490a943961d2093500
Nerivec <62446222+Nerivec@users.noreply.github.com>
2025-01-20 23:09:24 +0100

    Add OpenThread RCP firmware

--- tools/create_gbl.py
+++ tools/create_gbl.py
@@ -221,7 +221,7 @@ def main():
         openthread_config_h = parse_c_header_defines(
             (project_root / "config/sl_openthread_generic_config.h").read_text()
         )
-        metadata["ot_rcp_version"] = openthread_config_h["PACKAGE_STRING"]
+        metadata["ot_rcp_version"] = openthread_config_h["PACKAGE_VERSION"]
 
     if "gecko_bootloader_version" in gbl_dynamic:
         gbl_dynamic.remove("gecko_bootloader_version")
